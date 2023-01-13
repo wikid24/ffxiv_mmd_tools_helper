29266c3e2f823c3fb550b2b59c92505fc2b2b0f4
wikid24 <derekhakkim@gmail.com>
2023-01-13 12:33:15 -0800

    Add files via upload

---
+++ ffxiv_mmd_tools_helper_beta/boneMaps_renamer.py
@@ -0,0 +1,138 @@
+import bpy
+
+from . import register_wrap
+from . import model
+from . import import_csv
+
+@register_wrap
+class BonesRenamerPanel_MTH(bpy.types.Panel):
+	"""Creates the Bones Renamer Panel in a VIEW_3D TOOLS tab"""
+	bl_label = "Bones Renamer"
+	bl_idname = "OBJECT_PT_bones_renamer_MTH"
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "TOOLS" if bpy.app.version < (2,80,0) else "UI"
+	bl_category = "ffxiv_mmd_tools_helper"
+
+	def draw(self, context):
+		layout = self.layout
+		row = layout.row()
+
+		row.label(text="Mass Rename Bones", icon="ARMATURE_DATA")
+		row = layout.row()
+		row = layout.row()
+		layout.prop(context.scene, "Origin_Armature_Type")
+		row = layout.row()
+		layout.prop(context.scene, "Destination_Armature_Type")
+		row = layout.row()
+		row.operator("object.bones_renamer", text = "Mass Rename Bones")
+		row = layout.row()
+
+
+
+#use international fonts and display the names of the bones
+def use_international_fonts_display_names_bones():
+	bpy.context.object.data.show_names = True
+
+
+def unhide_all_armatures():
+	for o in bpy.context.scene.objects:
+		if o.type == 'ARMATURE':
+			o.hide = False
+
+def print_missing_bone_names():
+	missing_bone_names = []
+	BONE_NAMES_DICTIONARY = import_csv.use_csv_bones_dictionary()
+	FINGER_BONE_NAMES_DICTIONARY = import_csv.use_csv_bones_fingers_dictionary()
+	SelectedBoneMap = bpy.context.scene.Destination_Armature_Type
+	BoneMapIndex = BONE_NAMES_DICTIONARY[0].index(SelectedBoneMap)
+	FingerBoneMapIndex = FINGER_BONE_NAMES_DICTIONARY[0].index(SelectedBoneMap)
+	bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+	for b in BONE_NAMES_DICTIONARY:
+		if BONE_NAMES_DICTIONARY.index(b) != 0:
+			if b[BoneMapIndex] != '':
+				if b[BoneMapIndex] not in ["upper body 2", "上半身2"]:
+					if b[BoneMapIndex] not in bpy.context.active_object.data.bones.keys():
+						missing_bone_names.append(b[BoneMapIndex])
+	for b in FINGER_BONE_NAMES_DICTIONARY:
+		if FINGER_BONE_NAMES_DICTIONARY.index(b) != 0:
+			if b[FingerBoneMapIndex] != '':
+				if b[FingerBoneMapIndex] not in ["thumb0_L", "thumb0_R", "左親指0", "親指0.L", "右親指0", "親指0.R"]:
+					if b[FingerBoneMapIndex] not in bpy.context.active_object.data.bones.keys():
+						missing_bone_names.append(b[FingerBoneMapIndex])
+	print("\nBones renaming destination bone map was:")
+	print(SelectedBoneMap)
+	print("These bone names of" , SelectedBoneMap, "are missing from the active armature:" )
+	print(missing_bone_names)
+
+
+
+def rename_bones(boneMap1, boneMap2, BONE_NAMES_DICTIONARY): 
+	boneMaps = BONE_NAMES_DICTIONARY[0]
+	boneMap1_index = boneMaps.index(boneMap1)
+	boneMap2_index = boneMaps.index(boneMap2)
+	bpy.ops.object.mode_set(mode='OBJECT')
+
+	for k in BONE_NAMES_DICTIONARY[1:]:
+		if k[boneMap1_index] in bpy.context.active_object.data.bones.keys():
+			if k[boneMap2_index] != '':
+				bpy.context.active_object.data.bones[k[boneMap1_index]].name = k[boneMap2_index]
+				if boneMap2 == 'mmd_japanese' or boneMap2 == 'mmd_japaneseLR':
+					bpy.ops.object.mode_set(mode='POSE')
+					if hasattr(bpy.context.active_object.pose.bones[k[boneMap2_index]] , "mmd_bone"):
+						bpy.context.active_object.pose.bones[k[boneMap2_index]].mmd_bone.name_e = k[0]
+					bpy.ops.object.mode_set(mode='OBJECT')
+
+
+
+def rename_finger_bones(boneMap1, boneMap2, FINGER_BONE_NAMES_DICTIONARY):
+	boneMaps = FINGER_BONE_NAMES_DICTIONARY[0]
+	boneMap1_index = boneMaps.index(boneMap1)
+	boneMap2_index = boneMaps.index(boneMap2)
+	bpy.ops.object.mode_set(mode='OBJECT')
+
+	for k in FINGER_BONE_NAMES_DICTIONARY[1:]:
+		if k[boneMap1_index] in bpy.context.active_object.data.bones.keys():
+			if k[boneMap2_index] != '':
+				bpy.context.active_object.data.bones[k[boneMap1_index]].name = k[boneMap2_index]
+				if boneMap2 == 'mmd_japanese' or boneMap2 == 'mmd_japaneseLR':
+					bpy.ops.object.mode_set(mode='POSE')
+					if hasattr(bpy.context.active_object.pose.bones[k[boneMap2_index]] , "mmd_bone"):
+						bpy.context.active_object.pose.bones[k[boneMap2_index]].mmd_bone.name_e = k[0]
+					bpy.ops.object.mode_set(mode='OBJECT')
+
+	bpy.context.scene.Origin_Armature_Type = boneMap2
+	print_missing_bone_names()
+
+
+def main(context):
+	bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+
+	use_international_fonts_display_names_bones()
+	unhide_all_armatures()
+	BONE_NAMES_DICTIONARY = import_csv.use_csv_bones_dictionary()
+	FINGER_BONE_NAMES_DICTIONARY = import_csv.use_csv_bones_fingers_dictionary()
+	rename_bones(bpy.context.scene.Origin_Armature_Type, bpy.context.scene.Destination_Armature_Type, BONE_NAMES_DICTIONARY)
+	rename_finger_bones(bpy.context.scene.Origin_Armature_Type, bpy.context.scene.Destination_Armature_Type, FINGER_BONE_NAMES_DICTIONARY)
+	bpy.ops.object.mode_set(mode='POSE')
+	bpy.ops.pose.select_all(action='SELECT')
+
+
+@register_wrap
+class BonesRenamer(bpy.types.Operator):
+	"""Mass bones renamer for armature conversion"""
+	bl_idname = "object.bones_renamer"
+	bl_label = "Bones Renamer"
+
+	bpy.types.Scene.Origin_Armature_Type = bpy.props.EnumProperty(items = [('mmd_english', 'MMD English bone names', 'MikuMikuDance English bone names'), ('mmd_japanese', 'MMD Japanese bone names', 'MikuMikuDamce Japanese bone names'), ('mmd_japaneseLR', 'MMD Japanese bones names .L.R suffixes', 'MikuMikuDamce Japanese bones names with .L.R suffixes'), ('xna_lara', 'XNALara bone names', 'XNALara bone names'), ('daz_poser', 'DAZ/Poser bone names', 'DAZ/Poser/Second Life bone names'), ('blender_rigify', 'Blender rigify bone names', 'Blender rigify bone names before generating the complete rig'), ('sims_2', 'Sims 2 bone names', 'Sims 2 bone names'), ('motion_builder', 'Motion Builder bone names', 'Motion Builder bone names'), ('3ds_max', '3ds Max bone names', '3ds Max bone names'), ('bepu', 'Bepu full body IK bone names', 'Bepu full body IK bone names'), ('project_mirai', 'Project Mirai bone names', 'Project Mirai bone names'), ('manuel_bastioni_lab', 'Manuel Bastioni Lab bone names', 'Manuel Bastioni Lab bone names'), ('makehuman_mhx', 'Makehuman MHX bone names', 'Makehuman MHX bone names'), ('sims_3', 'Sims 3 bone names', 'Sims 3 bone names'), ('doa5lr', 'DOA5LR bone names', 'Dead on Arrival 5 Last Round bone names'), ('Bip_001', 'Bip001 bone names', 'Bip001 bone names'), ('biped_3ds_max', 'Biped 3DS Max bone names', 'Biped 3DS Max bone names'), ('biped_sfm', 'Biped Source Film Maker bone names', 'Biped Source Film Maker bone names'), ('valvebiped', 'ValveBiped bone names', 'ValveBiped bone names'), ('iClone7', 'iClone7 bone names', 'iClone7 bone names'),('ffxiv', 'ffxiv bone names', 'ffxiv bone names')], name = "Rename  bones  from :", default = 'ffxiv')
+
+
+	bpy.types.Scene.Destination_Armature_Type = bpy.props.EnumProperty(items = [('mmd_english', 'MMD English bone names', 'MikuMikuDance English bone names'), ('mmd_japanese', 'MMD Japanese bone names', 'MikuMikuDamce Japanese bone names'), ('mmd_japaneseLR', 'MMD Japanese bones names .L.R suffixes', 'MikuMikuDamce Japanese bones names with .L.R suffixes'), ('xna_lara', 'XNALara bone names', 'XNALara bone names'), ('daz_poser', 'DAZ/Poser bone names', 'DAZ/Poser/Second Life bone names'), ('blender_rigify', 'Blender rigify bone names', 'Blender rigify bone names before generating the complete rig'), ('sims_2', 'Sims 2 bone names', 'Sims 2 bone names'), ('motion_builder', 'Motion Builder bone names', 'Motion Builder bone names'), ('3ds_max', '3ds Max bone names', '3ds Max bone names'), ('bepu', 'Bepu full body IK bone names', 'Bepu full body IK bone names'), ('project_mirai', 'Project Mirai bone names', 'Project Mirai bone names'), ('manuel_bastioni_lab', 'Manuel Bastioni Lab bone names', 'Manuel Bastioni Lab bone names'), ('makehuman_mhx', 'Makehuman MHX bone names', 'Makehuman MHX bone names'), ('sims_3', 'Sims 3 bone names', 'Sims 3 bone names'), ('doa5lr', 'DOA5LR bone names', 'Dead on Arrival 5 Last Round bone names'), ('Bip_001', "Bip001 bone names", 'Bip001 bone names'), ('biped_3ds_max', 'Biped 3DS Max bone names', 'Biped 3DS Max bone names'), ('biped_sfm', 'Biped Source Film Maker bone names', 'Biped Source Film Maker bone names'), ('valvebiped', 'ValveBiped bone names', 'ValveBiped bone names'), ('iClone7', 'iClone7 bone names', 'iClone7 bone names'),('ffxiv', 'ffxiv bone names', 'ffxiv bone names')], name = "Rename  bones  to :", default = 'mmd_english')
+
+	#@classmethod
+	#def poll(cls, context):
+		#return context.active_object.type == 'ARMATURE'
+		#return context.active_object is not None
+
+	def execute(self, context):
+		main(context)
+		return {'FINISHED'}
---
+++ ffxiv_mmd_tools_helper_beta/miscellaneous_tools.py
@@ -0,0 +1,337 @@
+import bpy
+
+from . import register_wrap
+from . import model
+
+@register_wrap
+class MiscellaneousToolsPanel(bpy.types.Panel):
+	"""Miscellaneous Tools panel"""
+	bl_label = "Miscellaneous Tools Panel"
+	bl_idname = "OBJECT_PT_miscellaneous_tools"
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "TOOLS" if bpy.app.version < (2,80,0) else "UI"
+	bl_category = "ffxiv_mmd_tools_helper"
+
+	def draw(self, context):
+		layout = self.layout
+
+		row = layout.row()
+		layout.prop(context.scene, "selected_miscellaneous_tools")
+		row = layout.row()
+		row.label(text="Miscellaneous Tools", icon='WORLD_DATA')
+		row = layout.row()
+		row.operator("ffxiv_mmd_tools_helper.miscellaneous_tools", text = "Execute Function")
+
+def all_materials_mmd_ambient_white():
+	for m in bpy.data.materials:
+		if "mmd_tools_rigid" not in m.name:
+			m.mmd_material.ambient_color[0] == 1.0
+			m.mmd_material.ambient_color[1] == 1.0
+			m.mmd_material.ambient_color[2] == 1.0
+
+
+
+def combine_2_bones_1_bone(parent_bone_name, child_bone_name):
+	bpy.ops.object.mode_set(mode='EDIT')
+	child_bone_tail = bpy.context.active_object.data.edit_bones[child_bone_name].tail
+	bpy.context.active_object.data.edit_bones[parent_bone_name].tail = child_bone_tail
+	bpy.context.active_object.data.edit_bones.remove(bpy.context.active_object.data.edit_bones[child_bone_name])
+	bpy.ops.object.mode_set(mode='POSE')
+	print("Combined 2 bones: ", parent_bone_name, child_bone_name)
+
+def combine_2_vg_1_vg(parent_vg_name, child_vg_name):
+	for o in bpy.context.scene.objects:
+		if o.type == 'MESH':
+			if parent_vg_name in o.vertex_groups.keys():
+				if child_vg_name in o.vertex_groups.keys():
+					for v in o.data.vertices:
+						for g in v.groups:
+							if o.vertex_groups[g.group] == o.vertex_groups[child_vg_name]:
+								o.vertex_groups[parent_vg_name].add([v.index], o.vertex_groups[child_vg_name].weight(v.index), 'ADD')
+					o.vertex_groups.remove(o.vertex_groups[child_vg_name])
+					print("Combined 2 vertex groups: ", parent_vg_name, child_vg_name)
+
+def analyze_selected_parent_child_bone_pair():
+	selected_bones = []
+
+	for b in bpy.context.active_object.pose.bones:
+		if b.bone.select == True:
+			selected_bones.append(b.bone.name)
+
+	if len(selected_bones) != 2:
+		print("Exactly 2 bones must be selected." , len(selected_bones), "are selected.")
+		return None, None
+	if len(selected_bones) == 2:
+
+		if bpy.context.active_object.data.bones[selected_bones[0]].parent == bpy.context.active_object.data.bones[selected_bones[1]]:
+			parent_bone_name = selected_bones[1]
+			child_bone_name = selected_bones[0]
+			return parent_bone_name, child_bone_name
+			# combine_2_bones_1_bone(parent_bone, child_bone)
+			# combine_2_vg_1_vg(parent_bone, child_bone)
+
+		if bpy.context.active_object.data.bones[selected_bones[1]].parent == bpy.context.active_object.data.bones[selected_bones[0]]:
+			parent_bone_name = selected_bones[0]
+			child_bone_name = selected_bones[1]
+			return parent_bone_name, child_bone_name
+			# combine_2_bones_1_bone(parent_bone, child_bone)
+			# combine_2_vg_1_vg(parent_bone, child_bone)
+
+		if bpy.context.active_object.data.bones[selected_bones[0]].parent != bpy.context.active_object.data.bones[selected_bones[1]]:
+			if bpy.context.active_object.data.bones[selected_bones[1]].parent != bpy.context.active_object.data.bones[selected_bones[0]]:
+				print("Combining 2 bones to 1 bone requires a parent-child bone pair to be selected. There is no parent-child relationship between the 2 selected bones.")
+				return None, None
+
+	bpy.ops.object.mode_set(mode='POSE')
+
+def delete_unused_bones():
+	print('\n')
+	bpy.ops.object.mode_set(mode='EDIT')
+	bones_to_be_deleted = []
+
+	for b in bpy.context.active_object.data.edit_bones:
+		if 'unused' in b.name.lower():
+			bones_to_be_deleted.append(b.name)
+
+	for b in bones_to_be_deleted:
+		bpy.context.active_object.data.edit_bones.remove(bpy.context.active_object.data.edit_bones[b])
+		print("removed bone  ", b)
+
+	bpy.ops.object.mode_set(mode='POSE')
+
+def delete_unused_vertex_groups():
+	print('\n')
+	for o in bpy.context.scene.objects:
+		if o.type == 'MESH':
+			delete_these = []
+			for vg in o.vertex_groups:
+				if 'unused' in vg.name.lower():
+					if vg.name not in delete_these:
+						delete_these.append(vg.name)
+			for vg in delete_these:
+				if vg in o.vertex_groups.keys():
+					o.vertex_groups.remove(o.vertex_groups[vg])
+					print('removed vertex group  ', vg)
+
+def test_is_mmd_english_armature():
+	mmd_english = True
+	bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+	mmd_english_test_bone_names = ['upper body', 'neck', 'head', 'shoulder_L', 'arm_L', 'elbow_L', 'wrist_L', 'leg_L', 'knee_L', 'ankle_L', 'shoulder_R', 'arm_R', 'elbow_R', 'wrist_R', 'leg_R', 'knee_R', 'ankle_R']
+	missing_mmd_english_test_bone_names = []
+	for b in mmd_english_test_bone_names:
+		if b not in bpy.context.active_object.data.bones.keys():
+			missing_mmd_english_test_bone_names.append(b)
+	if len(missing_mmd_english_test_bone_names) > 0:
+		print("Missing mmd_english test bone names = ", missing_mmd_english_test_bone_names)
+		print('\n')
+		print("This armature appears not to be an mmd_english armature")
+		mmd_english = False
+	return mmd_english
+
+
+def correct_root_center():
+	print('\n')
+	if test_is_mmd_english_armature() == True:
+		bpy.ops.object.mode_set(mode='EDIT')
+
+		# if there is no "root" bone in the armature, a root bone is added
+		if "root" not in bpy.context.active_object.data.edit_bones.keys():
+			root_bone = bpy.context.active_object.data.edit_bones.new('root')
+			root_bone.head[:] = (0,0,0)
+			root_bone.tail[:] = (0,0.7,0)
+			if "center" in bpy.context.active_object.data.edit_bones.keys():
+				bpy.context.active_object.data.edit_bones["center"].parent = bpy.context.active_object.data.edit_bones["root"]
+				bpy.context.active_object.data.edit_bones["center"].use_connect = False
+			print("Added MMD root bone.")
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+		# if the "center" bone has a vertex group, it is renamed to "lower body"
+		#mesh_objects = model.find_MMD_MeshesList(bpy.context.active_object)
+		#for o in mesh_objects:
+		#	if "center" in o.vertex_groups.keys():
+		#		if "center" in bpy.context.active_object.data.bones.keys():
+		#			bpy.context.active_object.data.bones["center"].name = "lower body"
+		#			print("Renamed center bone to lower body bone.")
+		#			bpy.ops.object.mode_set(mode='EDIT')
+		#			bpy.context.active_object.data.edit_bones["lower body"].tail.z = 0.5 * (bpy.context.active_object.data.edit_bones["leg_L"].head.z + bpy.context.active_object.data.edit_bones["leg_R"].head.z)
+		#bpy.ops.object.mode_set(mode='OBJECT')
+
+		# if there is no "center" bone in the armature, a center bone is added
+		if "center" not in bpy.context.active_object.data.bones.keys():
+			bpy.ops.object.mode_set(mode='EDIT')
+			center_bone = bpy.context.active_object.data.edit_bones.new("center")
+			print("Added center bone.")
+			center_bone.head = 0.25 * (bpy.context.active_object.data.edit_bones["knee_L"].head + bpy.context.active_object.data.edit_bones["knee_R"].head + bpy.context.active_object.data.edit_bones["leg_L"].head + bpy.context.active_object.data.edit_bones["leg_R"].head)
+			center_bone.head.z = 0
+			center_bone.tail.y = center_bone.head.y - 0.7
+			center_bone.parent = bpy.context.active_object.data.edit_bones["root"]
+			if "lower body" in bpy.context.active_object.data.edit_bones.keys():
+				bpy.context.active_object.data.edit_bones["lower body"].parent = bpy.context.active_object.data.edit_bones["center"]
+			if "upper body" in bpy.context.active_object.data.edit_bones.keys():
+				bpy.context.active_object.data.edit_bones["upper body"].parent = bpy.context.active_object.data.edit_bones["center"]
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+	if test_is_mmd_english_armature() == False:
+		print("This operator will only work on an armature with mmd_english bone names. First rename bones to mmd_english and then try running this operator again.")
+
+
+def correct_groove():
+	print('\n')
+	if test_is_mmd_english_armature() == True:
+		bpy.ops.object.mode_set(mode='EDIT')
+
+			# if there is no "groove" bone in the armature, a groove bone is added
+		if "groove" not in bpy.context.active_object.data.bones.keys():
+			bpy.ops.object.mode_set(mode='EDIT')
+			groove = bpy.context.active_object.data.edit_bones.new("groove")
+			print("Added groove bone.")
+			groove.head = bpy.context.active_object.data.edit_bones["center"].head
+			groove.head.y = 0.01 + groove.head.y
+			groove.tail.y = 0.1 + (groove.head.y)
+			groove.parent = bpy.context.active_object.data.edit_bones["center"]
+			if "lower body" in bpy.context.active_object.data.edit_bones.keys():
+				bpy.context.active_object.data.edit_bones["lower body"].parent = bpy.context.active_object.data.edit_bones["groove"]
+			if "upper body" in bpy.context.active_object.data.edit_bones.keys():
+				bpy.context.active_object.data.edit_bones["upper body"].parent = bpy.context.active_object.data.edit_bones["groove"]
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+	if test_is_mmd_english_armature() == False:
+		print("This operator will only work on an armature with mmd_english bone names. First rename bones to mmd_english and then try running this operator again.")
+
+def correct_waist():
+	print('\n')
+	if test_is_mmd_english_armature() == True:
+		bpy.ops.object.mode_set(mode='EDIT')
+
+		# adjust the waist bone
+		bpy.ops.object.mode_set(mode='EDIT')
+		waist = bpy.context.active_object.data.edit_bones["waist"]
+		waist.tail = waist.head
+		waist.head.y = waist.tail.y - 0.05
+		waist.head.z = waist.tail.z - 0.03
+		waist.parent = bpy.context.active_object.data.edit_bones["groove"]
+		if "lower body" in bpy.context.active_object.data.edit_bones.keys():
+			bpy.context.active_object.data.edit_bones["lower body"].parent = bpy.context.active_object.data.edit_bones["waist"]
+		if "upper body" in bpy.context.active_object.data.edit_bones.keys():
+			bpy.context.active_object.data.edit_bones["upper body"].parent = bpy.context.active_object.data.edit_bones["waist"]
+		print("inverted the waist bone.")
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+	if test_is_mmd_english_armature() == False:
+		print("This operator will only work on an armature with mmd_english bone names. First rename bones to mmd_english and then try running this operator again.")
+
+def correct_view_cnt():
+	print('\n')
+	if test_is_mmd_english_armature() == True:
+		bpy.ops.object.mode_set(mode='EDIT')
+
+		# if there is no "view_cnt" bone in the armature, a root bone is added
+		if "view cnt" not in bpy.context.active_object.data.edit_bones.keys():
+			view_cnt = bpy.context.active_object.data.edit_bones.new('view cnt')
+			view_cnt.head[:] = (0,0,0)
+			view_cnt.tail[:] = (0,0.08,0)
+			print("Added MMD 'view cnt' bone.")
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+	if test_is_mmd_english_armature() == False:
+		print("This operator will only work on an armature with mmd_english bone names. First rename bones to mmd_english and then try running this operator again.")
+
+
+def create_bone_groups():
+	print('\n')
+	if test_is_mmd_english_armature() == True:
+		bpy.ops.object.mode_set(mode='EDIT')
+	
+	#create an 'IK' bone group and add the IK bones to it
+	bpy.ops.object.mode_set(mode='POSE')
+	if 'IK' not in bpy.context.active_object.pose.bone_groups.keys():
+		bpy.context.active_object.pose.bone_groups.new(name="IK")
+
+	bpy.context.active_object.pose.bones['leg IK_L'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['leg IK_R'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['toe IK_L'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['toe IK_R'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['leg IK_L_t'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['leg IK_R_t'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['toe IK_L_t'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+	bpy.context.active_object.pose.bones['toe IK_R_t'].bone_group = bpy.context.active_object.pose.bone_groups['IK']
+
+	#create an 'センター'(center) bone group and add the base bones to it
+	bpy.ops.object.mode_set(mode='POSE')
+	if 'センター' not in bpy.context.active_object.pose.bone_groups.keys():
+		bpy.context.active_object.pose.bone_groups.new(name="センター")
+	bpy.context.active_object.pose.bones['root'].bone_group = bpy.context.active_object.pose.bone_groups['センター']
+	bpy.context.active_object.pose.bones['center'].bone_group = bpy.context.active_object.pose.bone_groups['センター']
+	bpy.context.active_object.pose.bones['groove'].bone_group = bpy.context.active_object.pose.bone_groups['センター']
+	bpy.context.active_object.pose.bones['waist'].bone_group = bpy.context.active_object.pose.bone_groups['センター']
+
+	#create a 'root' bone group and add the 'view cnt' bone to it
+	bpy.ops.object.mode_set(mode='POSE')
+	if 'root' not in bpy.context.active_object.pose.bone_groups.keys():
+		bpy.context.active_object.pose.bone_groups.new(name="root")
+	bpy.context.active_object.pose.bones['view cnt'].bone_group = bpy.context.active_object.pose.bone_groups['root']
+
+
+	bpy.context.active_object.data.display_type = 'OCTAHEDRAL'
+	
+	if test_is_mmd_english_armature() == False:
+		print("This operator will only work on an armature with mmd_english bone names. First rename bones to mmd_english and then try running this operator again.")
+
+
+def main(context):
+	# print(bpy.context.scene.selected_miscellaneous_tools)
+	if bpy.context.scene.selected_miscellaneous_tools == "combine_2_bones":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		parent_bone_name, child_bone_name = analyze_selected_parent_child_bone_pair()
+		if parent_bone_name is not None:
+			if child_bone_name is not None:
+				combine_2_vg_1_vg(parent_bone_name, child_bone_name)
+				combine_2_bones_1_bone(parent_bone_name, child_bone_name)
+	if bpy.context.scene.selected_miscellaneous_tools == "delete_unused":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		delete_unused_bones()
+		delete_unused_vertex_groups()
+	if bpy.context.scene.selected_miscellaneous_tools == "mmd_ambient_white":
+		all_materials_mmd_ambient_white()
+	if bpy.context.scene.selected_miscellaneous_tools == "correct_root_center":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		correct_root_center()
+	if bpy.context.scene.selected_miscellaneous_tools == "correct_groove":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		correct_groove()
+	if bpy.context.scene.selected_miscellaneous_tools == "correct_waist":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		correct_waist()
+	if bpy.context.scene.selected_miscellaneous_tools == "correct_view_cnt":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		correct_view_cnt()
+	if bpy.context.scene.selected_miscellaneous_tools == "create_bone_groups":
+		bpy.context.view_layer.objects.active  = model.findArmature(bpy.context.active_object)
+		create_bone_groups()
+
+
+@register_wrap
+class MiscellaneousTools(bpy.types.Operator):
+	"""Miscellanous Tools"""
+	bl_idname = "ffxiv_mmd_tools_helper.miscellaneous_tools"
+	bl_label = "Miscellaneous Tools"
+
+	bpy.types.Scene.selected_miscellaneous_tools = bpy.props.EnumProperty(items = \
+	[('none', 'none', 'none')\
+	, ("combine_2_bones", "Combine 2 bones", "Combine a parent-child pair of bones and their vertex groups to 1 bone and 1 vertex group")\
+	, ("delete_unused", "Delete unused bones and unused vertex groups", "Delete all bones and vertex groups which have the word 'unused' in them")\
+	, ("mmd_ambient_white", "All materials MMD ambient color white", "Change the MMD ambient color of all materials to white")\
+	, ("correct_root_center", "Correct MMD Root and Center bones", "Correct MMD root and center bones")\
+	, ("correct_groove", "Correct MMD Groove bone", "Correct MMD Groove bone")\
+	, ("correct_waist", "Correct MMD Waist bone", "Correct MMD Waist bone")\
+	, ("correct_view_cnt", "Correct MMD 'view cnt' bone", "Correct MMD 'view cnt' bone")\
+	, ("create_bone_groups", "Create Bone Groups", "Create Bone Groups")\
+	], name = "Select Function:", default = 'none')
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None
+
+	def execute(self, context):
+		main(context)
+		return {'FINISHED'}

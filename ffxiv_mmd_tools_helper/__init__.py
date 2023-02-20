289d091527b218054a908a3aee1ab5db3e12dcc3
wikid24 <derekhakkim@gmail.com>
2023-02-19 21:48:33 -0800

    0.825

--- ffxiv_mmd_tools_helper/__init__.py
+++ ffxiv_mmd_tools_helper/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
 	"name": "FFXIV MMD Tools Helper",
 	"author": "wikid24",
-	"version": (0, 824),
+	"version": (0, 825),
 	"blender": (2, 80, 0),
 	"location": "View3D > Sidebar > FFXIV MMD Tools Helper",
 	"description": "Fork of MMDToolsHelper for FFXIV Models & updated Blender to be compatible with 2.8+",

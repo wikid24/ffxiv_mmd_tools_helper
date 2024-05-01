16801c5a03ce5c150e0c3809ecd5bde174c4b252
wikid24 <derekhakkim@gmail.com>
2024-04-30 23:01:32 -0700

    button label fix

--- ffxiv_mmd_tools_helper/export_bone_morphs.py
+++ ffxiv_mmd_tools_helper/export_bone_morphs.py
@@ -92,7 +92,7 @@ def export_bone_morph_data(filepath):
 # Operator to handle file selection using ExportHelper
 class ExportPoseBoneFileHelper(bpy.types.Operator, ExportHelper):
 	bl_idname = "ffxiv_mmd.export_bone_morphs_file"
-	bl_label = "Export Bone Morph(Pose Bone) Data to CSV"
+	bl_label = "Export Bone Morphs to CSV"
 
 	filename_ext = ".csv"
 
